6a82434b4e0d8b0f496b6ee6ef3f3767f9d96913
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2023-03-02 20:32:15 +0000

    Add class PageConverter

--- project/jekyll_to_obsidian_publish.py
+++ project/jekyll_to_obsidian_publish.py
@@ -4,33 +4,33 @@ from os import walk
 from os.path import join
 
 
-def convert_file(source_path: str, destination_path: str) -> None:
-    if source_path == './migration.md':
-        print(f'    Skipping {source_path}')
-        return
+class PageConverter:
+    def convert_file(self, source_path: str, destination_path: str) -> None:
+        if source_path == './migration.md':
+            print(f'    Skipping {source_path}')
+            return
 
-    with open(source_path) as f:
-        content = f.read()
+        with open(source_path) as f:
+            content = f.read()
 
-    # Hide the README.md from Publish, with frontmatter
-    if source_path == './README.md':
-        content = """---
+        # Hide the README.md from Publish, with frontmatter
+        if source_path == './README.md':
+            content = """---
 publish: false
 ---
 
 """ + content
 
-    content = convert_content(content)
+        content = self.convert_content(content)
 
-    from pathlib import Path
-    Path(os.path.dirname(destination_path)).mkdir(parents=True, exist_ok=True)
-    with open(destination_path, 'w') as f:
-        f.write(content)
+        from pathlib import Path
+        Path(os.path.dirname(destination_path)).mkdir(parents=True, exist_ok=True)
+        with open(destination_path, 'w') as f:
+            f.write(content)
 
+    def convert_content(self, content: str) -> str:
 
-def convert_content(content: str) -> str:
-
-    table_of_contents = """
+        table_of_contents = """
 <details open markdown="block">
   <summary>
     Table of contents
@@ -41,29 +41,29 @@ def convert_content(content: str) -> str:
 </details>
 """
 
-    table_of_contents_plus_rule = f'''
+        table_of_contents_plus_rule = f'''
 {table_of_contents}
 ---'''
 
-    replacements = [
-        ['{: .info }', '> [!info]'],
-        ['{: .released }', '> [!success] Released'],
-        ['{: .warning }', '> [!warning]'],
-        ['{: .no_toc }\n', ''],
-        [table_of_contents_plus_rule, ''],
-        [table_of_contents, ''],
-    ]
-    for replacement in replacements:
-        content = content.replace(replacement[0], replacement[1])
+        replacements = [
+            ['{: .info }', '> [!info]'],
+            ['{: .released }', '> [!success] Released'],
+            ['{: .warning }', '> [!warning]'],
+            ['{: .no_toc }\n', ''],
+            [table_of_contents_plus_rule, ''],
+            [table_of_contents, ''],
+        ]
+        for replacement in replacements:
+            content = content.replace(replacement[0], replacement[1])
 
-    # TODO Convert hyphens in #.... (heading names) to spaces
-    p = re.compile(r'\[([^{}]+)]\({{ site\.baseurl }}{% link ([a-z0-9-/]+)\.md %}(#[a-z-]+)?\)')
+        # TODO Convert hyphens in #.... (heading names) to spaces
+        p = re.compile(r'\[([^{}]+)]\({{ site\.baseurl }}{% link ([a-z0-9-/]+)\.md %}(#[a-z-]+)?\)')
 
-    lines = content.split('\n')
-    for i, line in enumerate(lines):
-        lines[i] = p.sub(r'[[\2\3|\1]]', line)
+        lines = content.split('\n')
+        for i, line in enumerate(lines):
+            lines[i] = p.sub(r'[[\2\3|\1]]', line)
 
-    return '\n'.join(lines)
+        return '\n'.join(lines)
 
 
 class SiteConverter:
@@ -84,6 +84,8 @@ class SiteConverter:
         For each markdown file that it finds, it replaces a particular comment line with the corresponding template.
         """
 
+        page_converter = PageConverter()
+
         for root, dirs, files in walk(self.source, topdown=True):
             # Exclude directories and files
             dirs[:] = [d for d in dirs if d not in ['_site']]
@@ -97,7 +99,7 @@ class SiteConverter:
                     destination_path = join(self.destination, source_path)
                     destination_path = os.path.normpath(destination_path)
                     print(destination_path)
-                    convert_file(source_path, destination_path)
+                    page_converter.convert_file(source_path, destination_path)
 
 
 def convert_markdown() -> None:
--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from approvaltests import Options
 from approvaltests.approvals import verify
 
-from project.jekyll_to_obsidian_publish import convert_content
+from project.jekyll_to_obsidian_publish import PageConverter
 
 
 class ConversionTests(unittest.TestCase):
@@ -15,15 +15,19 @@ class ConversionTests(unittest.TestCase):
         # Download DiffMerge at https://sourcegear.com/diffmerge/
 
     def test_info_conversion(self) -> None:
+        converter = PageConverter()
+
         input = f'''{{: .info }}
 > This is the information
 '''
         options = Options().for_file.with_extension(".md")
-        verify(convert_content(input), options=options)
+        verify(converter.convert_content(input), options=options)
 
     def test_full_conversion(self) -> None:
+        converter = PageConverter()
+
         input_file = Path(__file__).with_name('sample_jekyll_document.md')
         with open(input_file) as f:
             content = f.read()
         options = Options().for_file.with_extension(".md")
-        verify(convert_content(content), options=options)
+        verify(converter.convert_content(content), options=options)
